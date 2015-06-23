9c7c7bdda6246fe938251dac5ee0ecade5e2b7ff
Rui Zhao <oahziur@gmail.com>
2015-06-23 00:24:51 -0700

    refocus after moving window to another desktop

--- totalspaces2-script/move-to-space.rb
+++ totalspaces2-script/move-to-space.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
 require 'totalspaces2'
 
-to_space_id = ARGV[0].to_i
-TotalSpaces2.move_to_space(to_space_id)
+def main()
+  to_space_id = ARGV[0].to_i
+  TotalSpaces2.move_to_space(to_space_id)
+end
+
+main
--- totalspaces2-script/move-win-to-space.rb
+++ totalspaces2-script/move-win-to-space.rb
@@ -1,12 +1,32 @@
 #!/usr/bin/ruby
 require 'totalspaces2'
 
-to_space_id = ARGV[0].to_i
-windows = TotalSpaces2.window_list
-current_space = TotalSpaces2.current_space
-main_display_id = TotalSpaces2.main_display[:display_id]
-if !windows.empty?
+def front_windows?()
+  windows = TotalSpaces2.window_list
+  return !windows.empty?
+end
+
+def get_front_window_id(current_space, main_display_id)
+  windows = TotalSpaces2.window_list
   current_space_windows = windows.select {|window| window[:display_id] == main_display_id && window[:space_number] == current_space}
   front_window = current_space_windows[0]
-  TotalSpaces2.move_window_to_space(front_window[:window_id], to_space_id)
+  return front_window[:window_id]
 end
+
+def main()
+  to_space = ARGV[0].to_i
+  current_space = TotalSpaces2.current_space
+  main_display_id = TotalSpaces2.main_display[:display_id]
+  
+  if front_windows?
+    window_id = get_front_window_id(current_space, main_display_id)
+    TotalSpaces2.move_window_to_space(window_id, to_space)
+  end
+
+  if front_windows?
+    window_id = get_front_window_id(current_space, main_display_id)
+    TotalSpaces2.set_front_window(window_id)
+  end
+end
+
+main
